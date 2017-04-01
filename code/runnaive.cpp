3ea13c90667199c8f21c3d3c60379f776f69bc24
Stefan Toman <git@stoman.de>
2017-04-01 15:05:54 +0200

    add postprocessing

--- code/runnaive.cpp
+++ code/runnaive.cpp
@@ -2,7 +2,7 @@
 #include "util.cpp"
 #include "placerouters.cpp"
 #include "mst.cpp"
-
+#include "connectedcells.cpp"
 
 ll score_this_try(Input& input, vector<pair<int, int>>& pqrouters, int n, vector<int>& scores) {
 	vector<pair<int, int>> routersused(&pqrouters[0], &pqrouters[n - 1]);
@@ -45,18 +45,101 @@ void runnaive(Input& input, vector<pair<int, int>>& routers, vector<pair<int, in
 		}
 	}
 
-	//TODO if hi == lo + 1: which to take?
+	//if hi == lo + 1: which to take?
 	int res = lo;
 	if(lo + 1 < pqrouters.size() && score_this_try(input, pqrouters, lo+1, scores) > score_this_try(input, pqrouters, lo, scores)) {
 		res = lo + 1;
 	}
 	cerr << "Let's use " << res << " routers, sounds good..." << endl;
 
-	//return
+	//postprocessing
+	vector<vector<int>> countcovered(input.h, vector<int>(input.w, 0));
+	vector<bool> routerisused(pqrouters.size(), false);
 	for(int i = 0; i < res; i++) {
-		routers.push_back(pqrouters[i]);
+		routerisused[i] = true;
+		for(pair<int, int>& cell: connectedcells(input, pqrouters[i])) {
+			countcovered[cell.first][cell.second]++;
+		}
 	}
-	backbone = mst(input, routers);
+
+	int routertoremove = -1, routertoremovescore = -1;
+	int routertoadd= -1, routertoaddscore = -1;
+	do {
+		//find router to remove
+		routertoremove = -1;
+		routertoremovescore = -1;
+		for(int i = 0; i < pqrouters.size(); i++) {
+			if(routerisused[i]) {
+				int score = 0;
+				for(pair<int, int>& cell: connectedcells(input, pqrouters[i])) {
+					if(countcovered[cell.first][cell.second] == 1 && input.grid[cell.first][cell.second] == '.') {
+						score++;
+					}
+				}
+				if(routertoremove == -1 || score < routertoremovescore) {
+					routertoremove = i;
+					routertoremovescore = score;
+				}
+			}
+		}
+
+		//remove it
+		routerisused[routertoremove] = false;
+		for(pair<int, int>& cell: connectedcells(input, pqrouters[routertoremove])) {
+			countcovered[cell.first][cell.second]--;
+		}
+
+		//find router to add
+		routertoadd= -1;
+		routertoaddscore = -1;
+		for(int i = 0; i < pqrouters.size(); i++) {
+			if(!routerisused[i]) {
+				int score = 0;
+				for(pair<int, int>& cell: connectedcells(input, pqrouters[i])) {
+					if(countcovered[cell.first][cell.second] == 0 && input.grid[cell.first][cell.second] == '.') {
+						score++;
+					}
+				}
+				if(routertoadd == -1 || score > routertoaddscore) {
+					routertoadd = i;
+					routertoaddscore = score;
+				}
+			}
+		}
+
+		//add it
+		routerisused[routertoadd] = true;
+		for(pair<int, int>& cell: connectedcells(input, pqrouters[routertoadd])) {
+			countcovered[cell.first][cell.second]++;
+		}
+
+		cerr << "postprocessing improved our already huge score by " << (routertoaddscore - routertoremovescore) << " by removing " << routertoremove << " and adding " << routertoadd << ", its true, believe me" << endl;
+
+		//did we use too much money?
+		routers.clear();
+		for(int i = 0; i < pqrouters.size(); i++) {
+			if(routerisused[i]) {
+				routers.push_back(pqrouters[i]);
+			}
+		}
+		backbone = mst(input, routers);
+		if(backbone.size() * input.pb + routers.size() * input.pr > input.b) {
+			routerisused[routertoadd] = false;
+			routerisused[routertoremove] = true;
+
+			routers.clear();
+			for(int i = 0; i < pqrouters.size(); i++) {
+				if(routerisused[i]) {
+					routers.push_back(pqrouters[i]);
+				}
+			}
+			backbone = mst(input, routers);
+			cerr << "runnaive done with budget panic" << endl;
+			return;
+		}
+	}
+	while(routertoaddscore > routertoremovescore);
+
 	cerr << "runnaive done!" << endl;
 }
 
