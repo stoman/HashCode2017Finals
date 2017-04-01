0155292a570905a5327dc418d111048acd89f46a
Christian Müller <christian.mueller@tum.de>
2017-04-01 12:35:35 +0200

    add routers

--- code/placerouters.cpp
+++ code/placerouters.cpp
@@ -2,6 +2,7 @@
 #include "util.cpp"
 #include <queue>
 #include <vector>
+#include <algorithm>
 
 //give a list of routers to place, order by priority in descending order
 vector<pair<int, int>> placerouters(Input& input) {
@@ -19,15 +20,42 @@ vector<pair<int, int>> placerouters(Input& input) {
 	}
 
 	//compute routers
-	priority_queue<pair<int, pair<int, int>>> pq;//score, cell
+	vector<pair<int&, pair<int, int>>> pq;//score, cell
 	vector<pair<int, int>> routers;
 	vector<vector<bool>> covered(input.h, vector<bool>(input.w, false));
 
-	//fill pq with . cells
+	//fill pq with . or - cells with a score
+	for(int r = 0; r < input.h; r++) {
+		for(int c = 0; c < input.w; c++) {
+			if (input.grid[r][c] != "#" && score[r][c] > 0) {
+				pair<int, int> coord = make_pair(r, c);
+				pair<int&, pair<int, int>> tup = make_pair(score.at(r).at(c), coord);
+				pq.push_back(pq);
+			}
+		}
+	}
+	make_heap(pq.begin(), pq.end())
 
 	//loop until best score 0
+	while(!pq.empty() && pq.front().first > 0) {
 		//move best cell from `pq` to `routers`
-		//update, covered, scores, pq
+		//update covered, scores, pq
+		pair<int, pair<int, int>> tup = pq.front();
+		pop_heap(pq.begin(), pq.end());
+		pq.pop_back();
+
+		routers.push_back(tup.second);
+		for (pair<int, int>& cell : connectedcells(input, tup.second)) {
+			if (!covered.at(cell.first).at(cell.second)) {
+				covered.at(cell.first).at(cell.second) = true;
+
+				for (pair<int, int>& cell2 : connectedcells(input, cell)) {
+					score.at(cell2.first).at(cell2.second)--;
+				}
+			}
+		}
+		make_heap(vec.begin(), vec.end());
+	}
 
 	return routers;
 }
