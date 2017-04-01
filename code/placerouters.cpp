27c8fac89540d96b8080cf03d0632ca0d1d561f6
Stefan Toman <git@stoman.de>
2017-04-01 11:48:24 +0200

    add first part of placerouters

--- code/placerouters.cpp
+++ code/placerouters.cpp
@@ -3,5 +3,28 @@
 
 //give a list of routers to place, order by priority in descending order
 vector<pair<int, int>> placerouters(Input& input) {
-	//TODO fill me
+	//compute scores
+	vector<vector<int>> score(input.h, vector<int>(input.w, 0));
+	for(int r = 0; r < input.h; r++) {
+		for(int c = 0; c < input.w; c++) {
+			for(pair<int, int>& cell: connectedcells(input, make_pair(r, c))) {
+				if(input.grid[cell.first][cell.second] == '.') {
+					score[r][c]++;
+				}
+			}
+		}
+	}
+
+	//compute routers
+	priority_queue<pair<int, pair<int, int>>> pq;//score, cell
+	vector<pair<int, int>> routers;
+	vector<vector<bool>> covered(input.h, vector<bool>(input.w, false));
+
+	//fill pq with . cells
+
+	//loop until best score 0
+		//move best cell from `pq` to `routers`
+		//update, covered, scores, pq
+
+	return routers;
 }
